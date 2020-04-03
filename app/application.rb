239b72e5eb6eead5cde4972f2d81212915d7281c
Charles Pelton <charliejp0311@gmail.com>
2020-04-03 23:57:39 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -11,11 +11,23 @@ class Application
       @@items.each do |item|
         resp.write "#{item}\n"
       end
+
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
+
     elsif req.path.match(/cart/)
-      
+      if @@cart.empty?
+        resp.write "Your cart is empty"
+      else
+        @@cart.each do |item|
+          resp.write "#{item}\n"
+        end
+      end
+
+    elsif req.path.match(/add/)
+
+
     else
       resp.write "Path Not Found"
     end
